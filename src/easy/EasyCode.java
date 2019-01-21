b5d3fc831d2dc43a6a295abeaf0023912b7182c9
justxgy <justxgy@outlook.com>
2019-01-21 21:11:05 +0800

    初始化项目

---
+++ src/easy/EasyCode.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EasyCode {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * two sum
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * <p>
+     * Because nums[0] + nums[1] = 2 + 7 = 9,
+     * return [0, 1].
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - i;
+            if (map.containsKey(result)) {
+                return new int[]{map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
