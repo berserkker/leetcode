5a2523bc6ad6b9acd7d264820161e7ae31b35655
justxgy <justxgy@outlook.com>
2019-02-19 22:05:28 +0800

    增加快速排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -57,6 +57,7 @@ public class BubbleSort {
      */
     public static int[] straightInsertSort() {
         //外循环从第二个元素开始
+
         for (int i = 1; i < length; i++) {
             int key = array[i];
             int j;
@@ -82,6 +83,50 @@ public class BubbleSort {
 
     }
 
+    /**
+     * 快速排序 最好时间复杂度O(nlogn) 最坏时间复杂度O(n^2) 空间复杂度O(logn)~O(n)
+     * <p>
+     * 选一个数字，首位往中间比较，交换顺序，左边都是比他小的，右边都是比他大的
+     * 再递归，直到有序
+     */
+    public static void quickSort(int[] array, int low, int high) {
+        int pivot;
+        while (low < high) {
+            pivot = quickPartition(array, low, high);
+            quickSort(array, low, pivot - 1);
+            low = pivot + 1;
+        }
+    }
+
+    public static int quickPartition(int[] array, int low, int high) {
+        //选取枢轴，左右中间取三个数，选大小为中间值
+        int privotkey;
+        int m = low + (high - low) / 2;
+        if (array[low] > array[high]) {
+            swap(array, low, high);
+        }
+        if (array[m] > array[high]) {
+            swap(array, m, high);
+        }
+        if (array[m] > array[low]) {
+            swap(array, low, m);
+        }
+        privotkey = array[low];
+        while (low < high) {
+            while (low < high && array[high] >= privotkey) {
+                high--;
+            }
+            array[low] = array[high];
+
+            while (low < high && array[low] <= privotkey) {
+                low++;
+            }
+            array[high] = array[low];
+        }
+        array[low] = privotkey;
+        return low;
+    }
+
     public static void main(String[] args) {
         System.out.println("-----------冒泡排序------------");
         printArray(bubbleSort());
@@ -89,6 +134,16 @@ public class BubbleSort {
         printArray(simpleChooseSort());
         System.out.println("-----------直接插入排序------------");
         printArray(straightInsertSort());
+        System.out.println("-----------快速排序------------");
+        int[] quickArray = new int[]{9, 1, 5, 8, 1, 10, 3, 7, 4, 6, 2, 11, 0, 90, 99, 87, 10, 12, 1, 22, 3, 11, 44};
+        quickSort(quickArray, 0, quickArray.length - 1);
+        printArray(quickArray);
+    }
+
+    public static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 
     public static void printArray(int[] array) {
