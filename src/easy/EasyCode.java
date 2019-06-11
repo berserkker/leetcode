790ce1d2c55fa1290f1fbcf559ee3d3b335da5f6
berserkker <justxgy@outlook.com>
2019-06-11 23:31:21 +0800

    27一处元素

--- src/easy/EasyCode.java
+++ src/easy/EasyCode.java
@@ -195,56 +195,110 @@ public class EasyCode {
 
     /**
      * 20
-     *给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
-     *
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     * <p>
      * 有效字符串需满足：
-     *
+     * <p>
      * 左括号必须用相同类型的右括号闭合。
      * 左括号必须以正确的顺序闭合。
      * 注意空字符串可被认为是有效字符串。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: "()"
      * 输出: true
      * 示例 2:
-     *
+     * <p>
      * 输入: "()[]{}"
      * 输出: true
      * 示例 3:
-     *
+     * <p>
      * 输入: "(]"
      * 输出: false
      * 示例 4:
-     *
+     * <p>
      * 输入: "([)]"
      * 输出: false
      * 示例 5:
-     *
+     * <p>
      * 输入: "{[]}"
      * 输出: true
-     * */
-    public static boolean isvalid(String s){
+     */
+    public static boolean isvalid(String s) {
         Stack<String> stack = new Stack<>();
-        Map<String,String> map = new HashMap<String,String>(3){{
-            put(")","(");
-            put("}","{");
-            put("]","[");
+        Map<String, String> map = new HashMap<String, String>(3) {{
+            put(")", "(");
+            put("}", "{");
+            put("]", "[");
         }};
-        if("".equals(s)){
+        if ("".equals(s)) {
             return true;
         }
         for (char a : s.toCharArray()) {
             if (a == '(' || a == '{' || a == '[') {
                 stack.push(a + "");
-            }else if(!stack.isEmpty() && map.get(a+"").equals(stack.peek())){
+            } else if (!stack.isEmpty() && map.get(a + "").equals(stack.peek())) {
                 stack.pop();
-            }else {
+            } else {
                 return false;
             }
         }
 
         return stack.empty();
+    }
+
+    /**
+     * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 给定 nums = [3,2,2,3], val = 3,
+     * <p>
+     * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * 示例 2:
+     * <p>
+     * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+     * <p>
+     * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+     * <p>
+     * 注意这五个元素可为任意顺序。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * 说明:
+     * <p>
+     * 为什么返回数值是整数，但输出的答案是数组呢?
+     * <p>
+     * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * <p>
+     * 你可以想象内部操作如下:
+     * <p>
+     * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+     * int len = removeElement(nums, val);
+     * <p>
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+     * for (int i = 0; i < len; i++) {
+     *     print(nums[i]);
+     * }
+     * <p>
+     * 27  双指针法
+     */
+    public int removeElement(int[] nums, int val) {
+        int number = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[number] = nums[i];
+                number++;
+            }
+
+        }
+        return number;
     }
 }
 
